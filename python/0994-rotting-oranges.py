d39d8a75b7372630e7662df0313c2e138ea8a95c
samlin-2025 <lin.kaisong@gmail.com>
2026-06-10 12:51:51 -0500

    rotting oranges my version

--- python/0994-rotting-oranges.py
+++ python/0994-rotting-oranges.py
@@ -1,33 +1,37 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        q = collections.deque()
-        fresh = 0
-        time = 0
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        q = deque()
 
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == 1:
-                    fresh += 1
+        for r in range(rows):
+            for c in range(cols):
                 if grid[r][c] == 2:
                     q.append((r, c))
+                    visited.add((r,c))
+        
+        def infectNeighbors(r, c):
+            if r in range(rows) and c in range(cols) and (r, c) not in visited and grid[r][c] == 1:
+                grid[r][c] = 2
+                visited.add((r, c))
+                q.append((r, c))
 
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        while fresh > 0 and q:
-            length = len(q)
-            for i in range(length):
+
+        t = 0
+
+        while q:
+            for i in range(len(q)):
                 r, c = q.popleft()
+                infectNeighbors(r + 1, c)
+                infectNeighbors(r - 1, c)
+                infectNeighbors(r, c + 1)
+                infectNeighbors(r, c - 1)
+            if q: t += 1 #only count if new oranges were infected
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    return -1
 
-                for dr, dc in directions:
-                    row, col = r + dr, c + dc
-                    # if in bounds and nonrotten, make rotten
-                    # and add to q
-                    if (
-                        row in range(len(grid))
-                        and col in range(len(grid[0]))
-                        and grid[row][col] == 1
-                    ):
-                        grid[row][col] = 2
-                        q.append((row, col))
-                        fresh -= 1
-            time += 1
-        return time if fresh == 0 else -1
+
+        return t
