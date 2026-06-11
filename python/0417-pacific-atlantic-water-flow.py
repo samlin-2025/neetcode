5e92b1b1122ddc9c861c143a0e78b5fb0a2ce3d6
samlin-2025 <lin.kaisong@gmail.com>
2026-06-10 19:33:48 -0500

    417 my version

--- python/0417-pacific-atlantic-water-flow.py
+++ python/0417-pacific-atlantic-water-flow.py
@@ -1,35 +1,57 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
-        pac, atl = set(), set()
+        rows, cols = len(heights), len(heights[0])
+        pacific = set()
+        atlantic = set()
 
-        def dfs(r, c, visit, prevHeight):
-            if (
-                (r, c) in visit
-                or r < 0
-                or c < 0
-                or r == ROWS
-                or c == COLS
-                or heights[r][c] < prevHeight
-            ):
-                return
-            visit.add((r, c))
-            dfs(r + 1, c, visit, heights[r][c])
-            dfs(r - 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
-            dfs(r, c - 1, visit, heights[r][c])
+        q_pacific = deque()
+        q_atlantic = deque()
 
-        for c in range(COLS):
-            dfs(0, c, pac, heights[0][c])
-            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+        for r in range(rows):
+            q_pacific.append((r, 0))
+            q_atlantic.append((r, cols - 1))
+        
+        for c in range(cols):
+            q_pacific.append((0, c))
+            q_atlantic.append((rows - 1, c))
 
-        for r in range(ROWS):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+        def bfs_pacific(r, c):
+            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
 
+                if nr in range(rows) and nc in range(cols) and (nr, nc) not in pacific and heights[r][c] <= heights[nr][nc]:
+                    q_pacific.append((nr, nc))
+                    pacific.add((nr, nc))
+                
+        def bfs_atlantic(r, c):
+            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+
+                if nr in range(rows) and nc in range(cols) and (nr, nc) not in atlantic and heights[r][c] <= heights[nr][nc]:
+                    q_atlantic.append((nr, nc))
+                    atlantic.add((nr, nc))
+
+
+        while q_pacific:
+            for i in range(len(q_pacific)):
+                r, c = q_pacific.popleft()
+                pacific.add((r, c))
+                bfs_pacific(r, c)
+        
+
+        while q_atlantic:
+            for i in range(len(q_atlantic)):
+                r, c = q_atlantic.popleft()
+                atlantic.add((r, c))
+                bfs_atlantic(r, c)
         res = []
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r, c) in pac and (r, c) in atl:
-                    res.append([r, c])
+        for (r, c) in atlantic:
+            if (r, c) in pacific:
+                res.append([r, c])
+
         return res
+
+        
+        
