ae1a9fd4b237afe5062ca6dfde24554bb5febeee
samlin-2025 <lin.kaisong@gmail.com>
2026-06-11 21:30:04 -0500

    621 My version

--- python/0621-task-scheduler.py
+++ python/0621-task-scheduler.py
@@ -1,24 +1,24 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        count = Counter(tasks)
-        maxHeap = [-cnt for cnt in count.values()]
+        hashmap = Counter(tasks)
+        maxHeap = [-v for v in hashmap.values()]
         heapq.heapify(maxHeap)
 
         time = 0
-        q = deque()  # pairs of [-cnt, idleTime]
-        while maxHeap or q:
-            time += 1
-
-            if not maxHeap:
-                time = q[0][1]
-            else:
-                cnt = 1 + heapq.heappop(maxHeap)
-                if cnt:
+        q = deque()
+        while q or maxHeap:
+            time += 1;
+            if maxHeap:
+                cnt = -heapq.heappop(maxHeap) - 1
+                if cnt > 0:
                     q.append([cnt, time + n])
-            if q and q[0][1] == time:
-                heapq.heappush(maxHeap, q.popleft()[0])
-        return time
 
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, -q[0][0]) #always do a heappusgh
+                q.popleft()
+            
+        return time
+                
 
 # Greedy algorithm
 class Solution(object):
