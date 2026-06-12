8f525440d00e6e1f051a87cae4a3c427aae96456
samlin-2025 <lin.kaisong@gmail.com>
2026-06-12 17:14:06 -0500

    22 - my version

--- python/0022-generate-parentheses.py
+++ python/0022-generate-parentheses.py
@@ -1,21 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        stack = []
+        
         res = []
-
-        def backtrack(openN, closedN):
-            if openN == closedN == n:
-                res.append("".join(stack))
+        def dfs(currString, openCount, closedCount):
+            if openCount + closedCount == 2 * n:
+                res.append(currString)
                 return
+            
+            
+            if openCount < n:
+                dfs(currString + '(', openCount + 1, closedCount)
 
-            if openN < n:
-                stack.append("(")
-                backtrack(openN + 1, closedN)
-                stack.pop()
-            if closedN < openN:
-                stack.append(")")
-                backtrack(openN, closedN + 1)
-                stack.pop()
-
-        backtrack(0, 0)
+            if closedCount < openCount:
+                
+                dfs(currString + ')', openCount, closedCount + 1)
+            
+        dfs("", 0, 0)
         return res
+        
+                
